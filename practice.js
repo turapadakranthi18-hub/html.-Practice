f685f9f8858fdd0b6cc04e659a9704e2a83ca37a
Turapada Kranthi <turapadakranthi18@gmail.com>
2026-02-08 09:53:16 +0530

    java script file  created

---
+++ practice.js
@@ -0,0 +1,27 @@
+console.log("hello world")
+console.log("we are arthimetical")
+let a =100;
+let b =20;
+ 
+let c = a+b;
+console.log("addition of two numbers");
+console.log(c);
+ 
+let d = a-b;
+console.log("subtraction of two numbers");
+console.log(d);
+ 
+let e = a*b;
+console.log("multiplication of two numbers");
+console.log(e);
+ 
+let f = a/b;
+console.log("division of two numbers");
+console.log(f);
+const btn = document.getElementById("submitBtn");
+ 
+btn.addEventListener("click", () => {
+  console.log("clicked");
+})
+
+
