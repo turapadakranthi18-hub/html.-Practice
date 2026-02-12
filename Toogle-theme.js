aa6314eec66205561160d71e6525684177052a7a
Turapada Kranthi <turapadakranthi18@gmail.com>
2026-02-12 06:41:25 +0530

    toogle theme js files created

---
+++ Toogle-theme.js
@@ -0,0 +1,39 @@
+
+function addNumber(numberA, numberB) {
+    let sum = numberA + numberB;
+    console.log("The sum of two numbers is: " + sum);
+}
+addNumber(10, 20);
+addNumber(20, 30);
+addNumber(30, 40);
+addNumber(40, 50);
+addNumber(50, 60);
+
+function subtractNumber(numberA, numberB) {
+    let difference = numberA - numberB;
+    console.log(difference);
+}
+subtractNumber(10, 20);
+subtractNumber(20, 30);
+subtractNumber(30, 40);
+subtractNumber(40, 50);
+subtractNumber(50, 60);
+function multiplyNumber(numberA, numberB) {
+    let product = numberA * numberB;
+    console.log(product);
+}
+multiplyNumber(10, 20);
+multiplyNumber(20, 30);
+multiplyNumber(30, 40);
+multiplyNumber(40, 50);
+multiplyNumber(50, 60);
+function divideNumber(numberA, numberB) {
+    let quotient = numberA / numberB;                                                                                                                                                                     
+    console.log(quotient);
+}
+divideNumber(10, 20);
+divideNumber(20, 30);
+divideNumber(30, 40);
+divideNumber(40, 50);
+divideNumber(50, 60);
+
--- practice.js
+++ practice.js
@@ -1,3 +1,4 @@
+//functions
 console.log("hello world")
 console.log("we are arthimetical")
 let a =100;
@@ -18,10 +19,41 @@ console.log(e);
 let f = a/b;
 console.log("division of two numbers");
 console.log(f);
-const btn = document.getElementById("submitBtn");
+// const btn = document.getElementById("submitBtn");
  
-btn.addEventListener("click", () => {
-  console.log("clicked");
-})
+// btn.addEventListener("click", () => {
+//   console.log("clicked");
+// })
+// let students = ["John", "Jane", "Doe", "Smith"];
+// console.log(students);
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[3]);
+// let numbers = [10, 20, 30, 40, 50];
+// console.log(numbers);
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+// console.log(numbers[4]);
+//arrays
+let students = ["mahesh", "suresh", "ramesh", "suresh"];
+ 
+console.log(students.length);// array declaration
+ 
+for(let i = 0; i < students.length; i++){
+    console.log(students[i]);
+
+}
+//loops
+let classStudents = ["mahesh", "suresh", "ramesh", "suresh","kamesh","kranthi","prasad","nani"];
+let count=0;
+for(let i = 0; i < classStudents.length; i++){
+  if(classStudents[i] === "suresh"){
+  count++;
+  }
+}
+console.log(count);
 
 
